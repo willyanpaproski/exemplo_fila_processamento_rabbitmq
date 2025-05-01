7f2219667634d8f493611f078657b39fd310cc7e
Domwillys <willyanpaproski123@gmail.com>
2025-04-30 21:13:35 -0300

    First Commit

---
+++ ladoCliente.js
@@ -0,0 +1,30 @@
+const amqp = require("amqplib");
+
+async function sendEmails() {
+  const connection = await amqp.connect("amqp://root:root@localhost:5672");
+  const channel = await connection.createChannel();
+
+  const queue = "email_queue";
+  await channel.assertQueue(queue, { durable: true });
+
+  for (let i = 1; i <= 1000; i++) { // Aqui são enviadas as informações do email
+    const email = {
+      to: `cliente${i}@exemplo.com`,
+      subject: "Sua nota fiscal chegou!",
+      body: `Segue sua nota fiscal número ${i}`
+    };
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(email)), {
+      persistent: true
+    });
+
+    console.log(`E-mail #${i} enviado para a fila`);
+  }
+
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+}
+
+sendEmails();
---
+++ worker.js
@@ -0,0 +1,25 @@
+const amqp = require("amqplib");
+
+async function consumeEmails() {
+  const connection = await amqp.connect("amqp://root:root@localhost:5672");
+  const channel = await connection.createChannel();
+
+  const queue = "email_queue";
+  await channel.assertQueue(queue, { durable: true });
+
+  channel.prefetch(5); // Limita para 5 mensagens ao mesmo tempo
+
+  channel.consume(queue, (msg) => {
+    if (msg !== null) {
+      const email = JSON.parse(msg.content.toString()); //Aqui pode ser trabalhado para enviar um email da forma correta, estou enviando um json somente para simular
+      console.log(`📨 Enviando e-mail para: ${email.to}`);
+
+      console.log(`✅ E-mail enviado para: ${email.to}`);
+      channel.ack(msg);
+    }
+  });
+
+  console.log("Aguardando mensagens na fila...");
+}
+
+consumeEmails();
